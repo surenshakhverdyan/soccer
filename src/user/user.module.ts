0e5eb38a01817223a216c1bf54aa06d3bee17dfe
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-03 13:09:36 +0400

    import user model

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UserService } from './user.service';
+import { User, UserSchema } from 'src/schemas';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   providers: [UserService],
   exports: [UserService],
 })
