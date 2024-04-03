74b8eb2cffd0b0b4d70e5050f74f9785ff30e755
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-03 11:46:34 +0400

    transfer schema

--- src/schemas/transfer.schema.ts
+++ src/schemas/transfer.schema.ts
@@ -1,7 +1,34 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+import { Status } from 'src/enums';
 
 @Schema({ timestamps: true })
-export class Transfer extends Document {}
+export class Transfer extends Document {
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Player',
+  })
+  player: Types.ObjectId;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Team',
+  })
+  fromTeam: Types.ObjectId;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Team',
+  })
+  toTeam: Types.ObjectId;
+
+  @Prop({
+    type: String,
+    enum: Object.values(Status),
+    default: Status, // set value
+  })
+  status: string;
+}
 
 export const TransferSchema = SchemaFactory.createForClass(Transfer);
