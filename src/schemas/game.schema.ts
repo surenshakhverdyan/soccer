7958ddf78f528daf0a6effe37990f0255d847460
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-03 11:26:17 +0400

    game schema

--- src/schemas/game.schema.ts
+++ src/schemas/game.schema.ts
@@ -1,7 +1,87 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+import { Status } from 'src/enums';
 
 @Schema({ timestamps: true })
-export class Game extends Document {}
+export class Game extends Document {
+  @Prop({ type: Date })
+  startDateTime: Date;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'League',
+  })
+  league: Types.ObjectId;
+
+  @Prop({
+    type: {
+      team: { type: Types.ObjectId, ref: 'Team' },
+      players: [{ type: Types.ObjectId, ref: 'Player' }],
+      goals: [
+        {
+          assist: { type: Types.ObjectId, ref: 'Player' },
+          goal: { type: Types.ObjectId, ref: 'Player' },
+        },
+      ],
+      cards: [
+        {
+          player: { type: Types.ObjectId, ref: 'Player' },
+          yellow: { type: Number },
+          red: { type: Number },
+        },
+      ],
+    },
+  })
+  team1: {
+    team: Types.ObjectId;
+    players: Types.ObjectId[];
+    goals: Array<{ assist: Types.ObjectId; goal: Types.ObjectId }>;
+    cards: Array<{ player: Types.ObjectId; yellow: number; red: number }>;
+  };
+
+  @Prop({
+    type: {
+      team: { type: Types.ObjectId, ref: 'Team' },
+      players: [{ type: Types.ObjectId, ref: 'Player' }],
+      goals: [
+        {
+          assist: { type: Types.ObjectId, ref: 'Player' },
+          goal: { type: Types.ObjectId, ref: 'Player' },
+        },
+      ],
+      cards: [
+        {
+          player: { type: Types.ObjectId, ref: 'Player' },
+          yellow: { type: Number },
+          red: { type: Number },
+        },
+      ],
+    },
+  })
+  team2: {
+    team: Types.ObjectId;
+    players: Types.ObjectId[];
+    goals: Array<{ assist: Types.ObjectId; goal: Types.ObjectId }>;
+    cards: Array<{ player: Types.ObjectId; yellow: number; red: number }>;
+  };
+
+  @Prop({
+    type: String,
+  })
+  video: string;
+
+  @Prop({
+    type: [{ type: String }],
+  })
+  images: string[];
+
+  @Prop({
+    type: String,
+    enum: Object.values(Status),
+    default: Status, // set value
+  })
+  status: string;
+}
 
 export const GameSchema = SchemaFactory.createForClass(Game);
