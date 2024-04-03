0625a3d659d3e5f2b63642e766b6f6d2e6cd488b
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-03 11:44:03 +0400

    schedule schema

--- src/schemas/schedule.schema.ts
+++ src/schemas/schedule.schema.ts
@@ -1,7 +1,32 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema({ timestamps: true })
-export class Schedule extends Document {}
+export class Schedule extends Document {
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Game',
+  })
+  game: Types.ObjectId;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Team',
+  })
+  team: Types.ObjectId;
+
+  @Prop({
+    type: [
+      {
+        type: Types.ObjectId,
+        ref: 'Player',
+      },
+    ],
+  })
+  players: Types.ObjectId[];
+
+  @Prop({ type: Date })
+  date: Date;
+}
 
 export const ScheduleSchema = SchemaFactory.createForClass(Schedule);
