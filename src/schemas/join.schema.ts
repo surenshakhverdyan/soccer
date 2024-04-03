f35fc139bb9ae52cbc8816fb9da06e4e60924f93
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-03 11:49:07 +0400

    join schema

--- src/schemas/join.schema.ts
+++ src/schemas/join.schema.ts
@@ -1,7 +1,25 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
+import { Status } from 'src/enums';
+
 @Schema({ timestamps: true })
-export class Join extends Document {}
+export class Join extends Document {
+  @Prop({ type: String })
+  name: string;
+
+  @Prop({ type: String })
+  email: string;
+
+  @Prop({ type: String })
+  phone: string;
+
+  @Prop({
+    type: String,
+    enum: Object.values(Status),
+    default: Status, // set value
+  })
+  status: string;
+}
 
 export const JoinSchema = SchemaFactory.createForClass(Join);
