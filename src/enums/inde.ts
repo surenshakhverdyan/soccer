ebf78128b36267baf555070621e84abc8fea5194
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-03 08:19:24 +0400

    user schema

--- src/enums/inde.ts
+++
@@ -1,4 +0,0 @@
-export * from './position.enum';
-export * from './role.enum';
-export * from './status.enum';
-export * from './token-type.enum';
--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,7 +1,48 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+import { Role } from 'src/enums';
 
 @Schema({ timestamps: true })
-export class User extends Document {}
+export class User extends Document {
+  @Prop({
+    type: String,
+    required: true,
+  })
+  name: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    unique: true,
+  })
+  phone: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+    enum: Object.values(Role),
+    required: true,
+  })
+  role: string;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Team',
+  })
+  team: Types.ObjectId;
+}
 
 export const UserSchema = SchemaFactory.createForClass(User);
