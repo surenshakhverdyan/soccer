1caa6126373c51f9ebb06aae511b2e9a2c124c32
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-03 11:47:54 +0400

    cron schema

--- src/schemas/cron.schema.ts
+++ src/schemas/cron.schema.ts
@@ -1,7 +1,10 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema({ timestamps: true })
-export class Cron extends Document {}
+export class Cron extends Document {
+  @Prop({ type: Date })
+  endTransfers: Date;
+}
 
 export const CronSchema = SchemaFactory.createForClass(Cron);
