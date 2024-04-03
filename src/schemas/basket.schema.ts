4669aa4aff68b7d62015c9b5e437278b3bdec391
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-03 11:39:26 +0400

    basket schema

--- src/schemas/basket.schema.ts
+++ src/schemas/basket.schema.ts
@@ -1,7 +1,23 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema({ timestamps: true })
-export class Basket extends Document {}
+export class Basket extends Document {
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'League',
+  })
+  league: Types.ObjectId;
+
+  @Prop({
+    type: [
+      {
+        type: Types.ObjectId,
+        ref: 'Team',
+      },
+    ],
+  })
+  teams: Types.ObjectId[];
+}
 
 export const BasketSchema = SchemaFactory.createForClass(Basket);
