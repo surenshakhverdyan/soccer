ecc1011ca2da9fbc3c5ffd8d13899cd8f5185f28
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 11:50:49 +0400

    extend teams module

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { TeamsService } from './teams.service';
+import { Team, TeamSchema } from 'src/schemas';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Team.name, schema: TeamSchema }]),
+  ],
   providers: [TeamsService],
+  exports: [TeamsService],
 })
 export class TeamsModule {}
