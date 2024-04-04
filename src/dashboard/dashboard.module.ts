0346cb92de4c128ecbc39f0740a76946482dbd04
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 12:09:26 +0400

    extend dashboard team

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { TeamController } from './team/team.controller';
 import { TeamService } from './team/team.service';
 import { TeamsModule } from 'src/teams/teams.module';
 import { PlayersModule } from 'src/players/players.module';
+import { ImagesModule } from 'src/images/images.module';
 
 @Module({
-  imports: [TeamsModule, PlayersModule],
+  imports: [TeamsModule, PlayersModule, ImagesModule],
   controllers: [TeamController],
   providers: [TeamService],
 })
--- src/dashboard/team/team.service.ts
+++ src/dashboard/team/team.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { REQUEST } from '@nestjs/core';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Request } from 'express';
+import { Connection } from 'mongoose';
+
+import { PlayersService } from 'src/players/players.service';
+import { TeamsService } from 'src/teams/teams.service';
 
 @Injectable()
-export class TeamService {}
+export class TeamService {
+  constructor(
+    @InjectConnection() private readonly connection: Connection,
+    @Inject(REQUEST) private readonly request: Request,
+    private readonly teamsService: TeamsService,
+    private readonly playersService: PlayersService,
+  ) {}
+}
