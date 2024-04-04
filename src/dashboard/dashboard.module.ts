e4d74e45c2332e4f96c5c3c9f358e87a1db9467f
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 11:53:13 +0400

    update dashboard module

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+
 import { TeamController } from './team/team.controller';
 import { TeamService } from './team/team.service';
+import { TeamsModule } from 'src/teams/teams.module';
+import { PlayersModule } from 'src/players/players.module';
 
 @Module({
+  imports: [TeamsModule, PlayersModule],
   controllers: [TeamController],
   providers: [TeamService],
 })
