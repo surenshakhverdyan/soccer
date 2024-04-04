d3ecebbaf55029b5eb7ed6a1d71d912bf1f8eb72
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 19:23:48 +0400

    extend players module

---
+++ src/players/dto/player-create.dto.ts
@@ -0,0 +1,33 @@
+import {
+  ArrayUnique,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Types } from 'mongoose';
+
+import { Position } from 'src/enums';
+
+export class PlayerCreateDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @ArrayUnique()
+  @IsNotEmpty()
+  number: number;
+
+  @IsEnum(Position)
+  @IsNotEmpty()
+  position: string;
+
+  @IsString()
+  @IsOptional()
+  avatar?: string;
+
+  @IsOptional()
+  team?: Types.ObjectId;
+}
--- src/players/players.service.ts
+++ src/players/players.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Player } from 'src/schemas';
+import { PlayerCreateDto } from './dto';
 
 @Injectable()
-export class PlayersService {}
+export class PlayersService {
+  constructor(
+    @InjectModel(Player.name) private readonly playerModel: Model<Player>,
+  ) {}
+
+  async create(dto: PlayerCreateDto, session?: any): Promise<Player> {
+    const [player] = await this.playerModel.create([dto], session);
+
+    return player;
+  }
+}
