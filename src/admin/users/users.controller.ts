e5ce75098ed6b94fefae305a5969555fc807940b
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 11:00:37 +0400

    admin module

--- src/admin/users/users.controller.ts
+++ src/admin/users/users.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 
 import { AdminGuard } from 'src/guards';
 import { UsersService } from './users.service';
+import { UserCreateDto } from 'src/user/dto';
 
 @UseGuards(AdminGuard)
-@Controller('users')
+@Controller('admin')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post('create-user')
+  createUser(@Body() dto: UserCreateDto): Promise<boolean> {
+    return this.usersService.createUser(dto);
+  }
 }
--- src/admin/users/users.service.ts
+++ src/admin/users/users.service.ts
@@ -1,4 +1,35 @@
+import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
+
+import { welcomeTemplate } from 'src/templates';
+import { UserCreateDto } from 'src/user/dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly mailerService: MailerService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async createUser(dto: UserCreateDto): Promise<boolean> {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+    const template = welcomeTemplate(dto.email, dto.password);
+
+    dto.password = hashedPassword;
+
+    await this.userService.create(dto);
+
+    await this.mailerService.sendMail({
+      from: this.configService.get<string>('EMAIL_ADDRESS'),
+      to: dto.email,
+      subject: 'Welcome',
+      html: template,
+    });
+
+    return true;
+  }
+}
