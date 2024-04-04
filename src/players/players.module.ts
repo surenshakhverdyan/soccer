1c2c028a938a7bc28d03cfc4b4d6cf89194e9f2c
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 11:51:07 +0400

    extend players module

--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { PlayersService } from './players.service';
+import { Player, PlayerSchema } from 'src/schemas';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Player.name, schema: PlayerSchema }]),
+  ],
   providers: [PlayersService],
+  exports: [PlayersService],
 })
 export class PlayersModule {}
