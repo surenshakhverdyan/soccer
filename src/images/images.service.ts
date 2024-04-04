9e1af0e6d62a6a17f6177aa2fc922a8116ee099e
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 19:23:07 +0400

    update images service

--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { extname } from 'path';
+import * as fs from 'fs';
 
 @Injectable()
-export class ImagesService {}
+export class ImagesService {
+  async upload(image: Express.Multer.File): Promise<string> {
+    const fileName = `${
+      Date.now() + Math.floor(Math.random() * 99999)
+    }${extname(image.originalname)}`;
+
+    fs.writeFileSync(`uploads/${fileName}`, image.buffer);
+
+    return fileName;
+  }
+}
