ed1b8b975e1b1d35854498d3cc083829e36f1490
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 19:25:47 +0400

    test for dashboard

---
+++ test/dashboard-team.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+
+import { AppModule } from '../src/app.module';
+import { Position } from 'src/enums';
+
+describe('dashboard/team/TeamController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('create-team (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/team/create-team')
+      .set({
+        authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjBlNGY2Y2Q3OWRmYzJhZTc2N2U0OWIiLCJyb2xlIjoidXNlciIsInR5cGUiOiJBVCJ9.Q0s-_pC6bmUw_wFU1mOYSMiX7cBXSw4OO4sdPTyOALk',
+      })
+      .field('name', 'Soccer_1')
+      .field('players[0][name]', 'Henrik Mkhitaryan')
+      .field('players[0][number]', 10)
+      .field('players[0][position]', 'GK')
+      .expect(201);
+  });
+});
