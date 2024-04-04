9b61eec8a1446c953a5b4f1a4131d96a832413d8
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-04 11:10:21 +0400

    admin/user/test

---
+++ test/admin-user.e2e-spec.ts
@@ -0,0 +1,35 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+
+import { AppModule } from '../src/app.module';
+
+describe('admin/users/UsersController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('create-user (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/admin/create-user')
+      .set({
+        authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjBkNDM1ZWEzNjhiYzMxZDdlN2ZlYWMiLCJyb2xlIjoiYWRtaW4iLCJ0eXBlIjoiQVQifQ.Id7efVUe3F-WtxLYb8hvqpZBqgXVoxHsi5mnRirrBP8',
+      })
+      .send({
+        name: 'user_2',
+        email: 'user_2@mail.com',
+        phone: '+37411222222',
+        password: '156354111',
+        role: 'user',
+      })
+      .expect(201);
+  });
+});
