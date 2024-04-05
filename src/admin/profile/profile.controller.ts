bf842cc75709b206785f1ca4bc3679098753c9d0
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-05 11:51:15 +0400

    exend admin module

--- src/admin/profile/profile.controller.ts
+++ src/admin/profile/profile.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Patch, Put, UseGuards } from '@nestjs/common';
 
 import { AdminGuard } from 'src/guards';
 import { ProfileService } from './profile.service';
+import { UserUpdateDto } from 'src/users/dto';
+import { IUser } from 'src/users/interfaces';
 
 @UseGuards(AdminGuard)
-@Controller('profile')
+@Controller('admin')
 export class ProfileController {
   constructor(private readonly profileService: ProfileService) {}
+
+  @Put('update-profile')
+  updateProfile(@Body() dto: UserUpdateDto): Promise<IUser> {
+    return this.profileService.updateProfile(dto);
+  }
+
+  @Patch('password-update')
+  passwordUpdate(@Body() dto: UserUpdateDto): Promise<boolean> {
+    return this.profileService.passwordUpdate(dto);
+  }
 }
