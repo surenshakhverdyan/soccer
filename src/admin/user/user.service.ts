2fcfab2efb8c1abfc7add324c8ffedbe45f2b45d
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-05 22:55:35 +0400

    extend

--- src/admin/user/user.service.ts
+++ src/admin/user/user.service.ts
@@ -1,18 +1,25 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection, Types } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 
+import { TeamsService } from 'src/teams/teams.service';
 import { welcomeTemplate } from 'src/templates';
 import { UserCreateDto } from 'src/users/dto';
 import { UsersService } from 'src/users/users.service';
+import { PlayersService } from 'src/players/players.service';
 
 @Injectable()
 export class UserService {
   constructor(
+    @InjectConnection() private readonly connection: Connection,
     private readonly usersService: UsersService,
     private readonly mailerService: MailerService,
     private readonly configService: ConfigService,
+    private readonly teamsService: TeamsService,
+    private readonly playersService: PlayersService,
   ) {}
 
   async createUser(dto: UserCreateDto): Promise<boolean> {
@@ -31,5 +38,27 @@ export class UserService {
     });
 
     return true;
+  }
+
+  async deleteUser(userId: Types.ObjectId): Promise<boolean> {
+    const session = await this.connection.startSession();
+
+    try {
+      session.startTransaction();
+
+      const _user = await this.usersService.delete(userId);
+      await this.teamsService.delete(_user.team);
+      await this.playersService.deleteMany(_user.team, session);
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return true;
+    } catch (error: any) {
+      await session.abortTransaction();
+      session.endSession();
+
+      throw new HttpException(error.message, 500);
+    }
   }
 }
