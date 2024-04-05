6c3cf16390aa77b82b1814b34af0dcb57f29bea0
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-05 14:41:05 +0400

    create transfer test

---
+++ test/dashboard-transfer.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+
+import { AppModule } from '../src/app.module';
+
+describe('dashboard/transfer/TransferController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('update profile (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/transfer/create-transfer')
+      .set({
+        authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NjBlNGY2Y2Q3OWRmYzJhZTc2N2U0OWIiLCJyb2xlIjoidXNlciIsInR5cGUiOiJBVCJ9.Q0s-_pC6bmUw_wFU1mOYSMiX7cBXSw4OO4sdPTyOALk',
+      })
+      .send({
+        playerId: '660f9f416307a97a35ced524',
+      })
+      .expect(201);
+  });
+});
