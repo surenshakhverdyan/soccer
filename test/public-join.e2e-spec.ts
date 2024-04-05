32a9cae75016d908d73c6f309306663296285e96
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-05 17:09:43 +0400

    join test

---
+++ test/public-join.e2e-spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+
+import { AppModule } from '../src/app.module';
+
+describe('public/join/JoinController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('join (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/join')
+      .send({
+        name: 'Joiner',
+        email: 'test@email.com',
+        phone: '+37411999654',
+      })
+      .expect(201);
+  });
+});
