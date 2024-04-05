36706fcb392e6087efddd99d518d3004f433018d
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-05 10:51:45 +0400

    extend

--- src/dashboard/user/user.controller.ts
+++ src/dashboard/user/user.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Body, Controller, Patch, Put, UseGuards } from '@nestjs/common';
 
 import { AuthGuard } from 'src/guards';
 import { UserService } from './user.service';
+import { UserUpdateDto } from 'src/users/dto';
+import { IUser } from 'src/users/interfaces';
 
 @UseGuards(AuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Put('update-profile')
+  updateProfile(@Body() dto: UserUpdateDto): Promise<IUser> {
+    return this.userService.updateProfile(dto);
+  }
+
+  @Patch('password-update')
+  passwordUpdate(@Body() dto: UserUpdateDto): Promise<boolean> {
+    return this.userService.passwordUpdate(dto);
+  }
 }
