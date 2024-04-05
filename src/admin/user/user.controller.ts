404be2af19a79c3be93becf21911a3f6060ea15e
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-05 23:03:20 +0400

    extend

--- src/admin/user/user.controller.ts
+++ src/admin/user/user.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Post, UseGuards } from '@nestjs/common';
 
 import { AdminGuard } from 'src/guards';
 import { UserService } from './user.service';
 import { UserCreateDto } from 'src/users/dto';
+import { Types } from 'mongoose';
 
 @UseGuards(AdminGuard)
 @Controller('admin')
@@ -12,5 +13,10 @@ export class UserController {
   @Post('create-user')
   createUser(@Body() dto: UserCreateDto): Promise<boolean> {
     return this.userService.createUser(dto);
+  }
+
+  @Delete('delete-user')
+  deleteUser(@Body('userId') userId: Types.ObjectId): Promise<boolean> {
+    return this.userService.deleteUser(userId);
   }
 }
