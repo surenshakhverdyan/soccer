8ca83d6f045cc7fdf1b88e14d7f5cd76843c2b01
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-07 13:22:05 +0400

    extend

--- src/leagues/leagues.service.ts
+++ src/leagues/leagues.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { ClientSession, Model, Types } from 'mongoose';
 
 import { League } from 'src/schemas';
 import { LeagueCreateDto } from './dto';
@@ -13,6 +13,20 @@ export class LeaguesService {
 
   async create(dto: LeagueCreateDto): Promise<League> {
     const league = await this.leagueModel.create(dto);
+
+    return league;
+  }
+
+  async addBasket(
+    leagueId: Types.ObjectId,
+    basketId: Types.ObjectId,
+    session?: ClientSession,
+  ): Promise<League> {
+    const league = await this.leagueModel.findByIdAndUpdate(
+      leagueId,
+      { $push: { baskets: basketId } },
+      { new: true, session },
+    );
 
     return league;
   }
