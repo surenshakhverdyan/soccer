a12e29544da67553fea74bd79ee0a371b60472b8
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-07 12:51:32 +0400

    extend

--- src/baskets/baskets.service.ts
+++ src/baskets/baskets.service.ts
@@ -3,10 +3,17 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import { Basket } from 'src/schemas';
+import { BasketCreateDto } from './dto';
 
 @Injectable()
 export class BasketsService {
   constructor(
     @InjectModel(Basket.name) private readonly basketModel: Model<Basket>,
   ) {}
+
+  async create(dto: BasketCreateDto): Promise<Basket> {
+    const basket = await this.basketModel.create(dto);
+
+    return basket;
+  }
 }
