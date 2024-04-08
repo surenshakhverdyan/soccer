9f26041358537db2e15f1131f593482d4cad91c3
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-08 12:21:41 +0400

    update admin game

--- src/admin/game/game.service.ts
+++ src/admin/game/game.service.ts
@@ -1,12 +1,16 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { HttpException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
-import { BasketsService } from 'src/baskets/baskets.service';
 
+import { BasketsService } from 'src/baskets/baskets.service';
+import { Role, TokenType } from 'src/enums';
 import { GameCreateDto } from 'src/games/dto';
 import { GamesService } from 'src/games/games.service';
 import { Game } from 'src/schemas';
+import { scheduleGameTemplate } from 'src/templates';
+import { TokenService } from 'src/token/token.service';
 
 @Injectable()
 export class GameService {
@@ -15,29 +19,47 @@ export class GameService {
     private readonly gamesService: GamesService,
     private readonly mailerService: MailerService,
     private readonly basketsService: BasketsService,
+    private readonly tokenService: TokenService,
+    private readonly configService: ConfigService,
   ) {}
 
   async createGame(dto: GameCreateDto): Promise<Game> {
-    // const session = await this.connection.startSession();
+    const session = await this.connection.startSession();
 
     try {
-      // session.startTransaction();
+      session.startTransaction();
 
       const basket = await this.basketsService.getById(dto.basket);
 
-      basket.teams.map((team) => {
-        console.log(team);
-      });
-
       const game = await this.gamesService.create(dto);
 
-      // await session.commitTransaction();
-      // session.endSession();
+      basket.teams.map(async (team) => {
+        const payload = {
+          sub: `${team._id} ${game._id}`,
+          role: Role.User,
+          type: TokenType.GameScheduleToken,
+        };
+        const token = this.tokenService.signGameScheduleToken(payload);
+        const url = `${this.configService.get<string>(
+          'BASE_URL',
+        )}/schedule-game/${token}`;
+        const template = scheduleGameTemplate(url);
+
+        await this.mailerService.sendMail({
+          from: this.configService.get<string>('EMAIL_ADDRESS'),
+          to: team.user.email,
+          subject: 'Game scheduler',
+          html: template,
+        });
+      });
+
+      await session.commitTransaction();
+      session.endSession();
 
       return game;
     } catch (error: any) {
-      // await session.abortTransaction();
-      // session.endSession();
+      await session.abortTransaction();
+      session.endSession();
 
       throw new HttpException(error.message, 500);
     }
