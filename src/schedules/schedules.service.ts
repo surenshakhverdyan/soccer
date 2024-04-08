4568ad07dcaea05229e4324a1b2574e638927714
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-08 13:49:31 +0400

    extend

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 import { Schedule } from 'src/schemas';
 import { ScheduleCreateDto } from './dto';
@@ -13,6 +13,18 @@ export class SchedulesService {
 
   async create(dto: ScheduleCreateDto): Promise<Schedule> {
     const schedule = await this.scheduleModel.create(dto);
+
+    return schedule;
+  }
+
+  async getByTeamAndGameIds(
+    teamId: Types.ObjectId,
+    gameId: Types.ObjectId,
+  ): Promise<Schedule> {
+    const schedule = await this.scheduleModel.findOne({
+      team: teamId,
+      game: gameId,
+    });
 
     return schedule;
   }
