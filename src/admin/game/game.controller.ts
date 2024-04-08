18d8a6eb68fab2cdaed371660fd730b29dff35b4
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-09 00:12:07 +0400

    exend admin game

--- src/admin/game/game.controller.ts
+++ src/admin/game/game.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
 
 import { AdminGuard } from 'src/guards';
 import { GameService } from './game.service';
 import { GameCreateDto } from 'src/games/dto';
 import { Game } from 'src/schemas';
+import { GameUpdateDto } from './dto';
 
 @UseGuards(AdminGuard)
 @Controller('admin')
@@ -13,5 +14,10 @@ export class GameController {
   @Post('create-game')
   createGame(@Body() dto: GameCreateDto): Promise<Game> {
     return this.gameService.createGame(dto);
+  }
+
+  @Put('set-game')
+  setGame(@Body() dto: GameUpdateDto): Promise<Game> {
+    return this.gameService.setGame(dto);
   }
 }
--- src/admin/game/game.service.ts
+++ src/admin/game/game.service.ts
@@ -5,15 +5,16 @@ import { InjectConnection } from '@nestjs/mongoose';
 import { Connection, Types } from 'mongoose';
 
 import { BasketsService } from 'src/baskets/baskets.service';
-import { Role, TokenType } from 'src/enums';
+import { Role, Status, TokenType } from 'src/enums';
 import { GameCreateDto } from 'src/games/dto';
 import { GamesService } from 'src/games/games.service';
 import { LeaguesService } from 'src/leagues/leagues.service';
 import { Game } from 'src/schemas';
-import { scheduleGameTemplate } from 'src/templates';
+import { gameDateTimeTemplate, scheduleGameTemplate } from 'src/templates';
 import { TokenService } from 'src/token/token.service';
 import { GameUpdateDto } from './dto';
 import { PlayersService } from 'src/players/players.service';
+import { SchedulesService } from 'src/schedules/schedules.service';
 
 @Injectable()
 export class GameService {
@@ -26,6 +27,7 @@ export class GameService {
     private readonly configService: ConfigService,
     private readonly leaguesService: LeaguesService,
     private readonly playersService: PlayersService,
+    private readonly schedulesService: SchedulesService,
   ) {}
 
   async createGame(dto: GameCreateDto): Promise<Game> {
@@ -75,20 +77,55 @@ export class GameService {
   }
 
   async setGame(dto: GameUpdateDto): Promise<Game> {
-    const team_1 = await this.basketsService.getById(dto.team_1);
-    const team_2 = await this.basketsService.getById(dto.team_2);
-    const data = {
-      team_1,
-      team_2,
-      startDateTime: dto.startDateTime,
-    };
-
+    const team_1 = await this.schedulesService.getById(dto.team_1);
+    const team_2 = await this.schedulesService.getById(dto.team_2);
     const session = await this.connection.startSession();
 
     try {
-      
+      session.startTransaction();
+
+      const data = {
+        gameId: team_1.game,
+        team_1: {
+          team: team_1.team._id,
+          players: team_1.players,
+        },
+        team_2: {
+          team: team_2.team._id,
+          players: team_2.players,
+        },
+        startDateTime: dto.startDateTime,
+        status: Status.Active,
+      };
+
+      const game = await this.gamesService.setGame(data, session);
+      await this.playersService.findByTeamAndActivate(team_1.team._id, session);
+      await this.playersService.findByTeamAndActivate(team_2.team._id, session);
+
+      const template = gameDateTimeTemplate({
+        dateTime: data.startDateTime,
+        team_1: team_1.team.name,
+        team_2: team_2.team.name,
+      });
+      const emails = [team_1.team.user.email, team_2.team.user.email];
+      for (let i = 0; i < emails.length; i++) {
+        await this.mailerService.sendMail({
+          from: this.configService.get<string>('EMAIL_ADDRESS'),
+          to: emails[i],
+          subject: 'Game date and time',
+          html: template,
+        });
+      }
+
+      await session.commitTransaction();
+      session.endSession();
+
+      return game;
     } catch (error: any) {
-      
+      await session.abortTransaction();
+      session.endSession();
+
+      throw new HttpException(error.message, 500);
     }
   }
 }
