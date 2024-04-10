cbc71d7ddc52e2dd2a84048658986bfe662f3c97
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-04-10 11:23:56 +0400

    update dto

--- src/admin/game/dto/game-set.dto.ts
+++ src/admin/game/dto/game-set.dto.ts
@@ -3,10 +3,10 @@ import { Types } from 'mongoose';
 
 export class GameSetDto {
   @IsNotEmpty()
-  team_1: Types.ObjectId;
+  team_1: Types.ObjectId; // schedule ids
 
   @IsNotEmpty()
-  team_2: Types.ObjectId;
+  team_2: Types.ObjectId; // schedule ids
 
   @IsDateString()
   startDateTime: Date;
