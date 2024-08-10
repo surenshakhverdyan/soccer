4c396339bdd8166c8f289ecb18ffd7a829ca37cb
Suren Hakhverdyan <surenshakhverdyan@gmail.com>
2024-08-10 16:48:43 +0400

    to roll back

--- src/enums/position.enum.ts
+++ src/enums/position.enum.ts
@@ -1,7 +1,7 @@
 export enum Position {
   GK = 'Goalkeeper',
   CB = 'Center Defender',
-  RB = 'Right Defender',
+  RB = 'Right Defende',
   LB = 'Left Defender',
   CM = 'Center Midfielder',
   RM = 'Right Midfielder',
